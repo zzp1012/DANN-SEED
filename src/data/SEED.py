af9a17ce1b826e4da6e9a918ca2dc2094e8db283
zzp1012 <zzp1012@sjtu.edu.cn>
2022-06-30 03:49:05 +0800

    update: the SEED dataset

--- src/data/SEED.py
+++ src/data/SEED.py
@@ -1,9 +1,39 @@
 import pickle
+from re import X
 import numpy as np
 import torch
+from torch.utils.data import Dataset
 
 # import internal libs
 from utils import get_logger
+
+class SEEDDataset(Dataset):
+    """SEED dataset."""
+
+    def __init__(self, 
+                 X: np.ndarray, 
+                 y: np.ndarray):
+        """
+        Args:
+            X (np.ndarray): the input data.
+            y (np.ndarray): the target data
+        """
+        self.X = X
+        self.y = y
+        if not isinstance(self.X, torch.Tensor):
+            self.X = torch.from_numpy(self.X).float()
+        if not isinstance(self.y, torch.Tensor):
+            self.y = torch.from_numpy(self.y).long()
+
+    def __len__(self):
+        assert len(self.X) == len(self.y)
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        return self.X[idx], self.y[idx]
+
 
 def load(root: str = "../data/SEED/data.pkl",
          val_domain: int = 0) -> tuple:
@@ -32,15 +62,12 @@ def load(root: str = "../data/SEED/data.pkl",
     train_Y = np.hstack([v['label'] for k, v in data.items() if k != val_domain])
     val_X = data[val_domain]["data"]
     val_Y = data[val_domain]["label"]
-
-    # convert to torch.Tensor
-    train_X = torch.from_numpy(train_X).float()
-    train_Y = torch.from_numpy(train_Y).long()
-    val_X = torch.from_numpy(val_X).float()
-    val_Y = torch.from_numpy(val_Y).long()
-
+    
     # basic info
     logger.info(f"train_X shape: {train_X.shape}; train_Y shape: {train_Y.shape}")
     logger.info(f"val_X shape: {val_X.shape}; val_Y shape: {val_Y.shape}")
-    
-    return (train_X, train_Y), (val_X, val_Y)
+
+    # get the train set and test set
+    train_set = SEEDDataset(train_X, train_Y)
+    val_set = SEEDDataset(val_X, val_Y)
+    return train_set, val_set
