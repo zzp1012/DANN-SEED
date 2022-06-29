5219cd369006d28954088e9b56e35bf91984133f
zzp1012 <zzp1012@sjtu.edu.cn>
2022-06-30 01:17:42 +0800

    update: add the basic structure

---
+++ src/main.py
@@ -0,0 +1,80 @@
+import os
+import argparse
+import torch
+
+# import internal libs
+from utils import set_logger, get_logger, set_seed, set_device, \
+    log_settings, save_current_src
+from config import DATE, MOMENT, SRC_PATH
+
+def add_args() -> argparse.Namespace:
+    """get arguments from the program.
+
+    Returns:
+        return a dict containing all the program arguments 
+    """
+    parser = argparse.ArgumentParser(
+        description="Emotion Recognition with DANN")
+    ## the basic setting of exp
+    parser.add_argument('--device', default=0, type=int,
+                        help="set the device.")
+    parser.add_argument("--seed", default=0, type=int,
+                        help="set the random seed.")
+    parser.add_argument("--save_root", default="../outs/tmp/", type=str,
+                        help='the path of saving results.')
+    parser.add_argument("--dataset", default="SEED", type=str,
+                        help='the dataset name.')
+    parser.add_argument("--model", default="DANN", type=str,
+                        help='the model name.')
+    parser.add_argument('--epochs', default=100, type=int,
+                        help="set epoch number")
+    parser.add_argument("--lr", default=0.01, type=float,
+                        help="set the learning rate.")
+    parser.add_argument("--bs", default=128, type=int,
+                        help="set the batch size")
+    # set if using debug mod
+    parser.add_argument("-v", "--verbose", action="store_true", dest="verbose",
+                        help="enable debug info output.")
+    args = parser.parse_args()
+
+    if not os.path.exists(args.save_root):
+        os.makedirs(args.save_root)
+
+    # set the save_path
+    exp_name = "-".join([DATE, 
+                         MOMENT,
+                         f"seed{args.seed}",
+                         f"{args.dataset}",
+                         f"{args.model}",
+                         f"epochs{args.epochs}",
+                         f"lr{args.lr}",
+                         f"bs{args.bs}",])
+    args.save_path = os.path.join(args.save_root, exp_name)
+    if not os.path.exists(args.save_path):
+        os.makedirs(args.save_path)
+
+    return args
+
+def main():
+    # get the args.
+    args = add_args()
+    # set the logger
+    set_logger(args.save_path)
+    # get the logger
+    logger = get_logger(__name__, args.verbose)
+    # set the seed
+    set_seed(args.seed)
+    # set the device
+    args.device = set_device(args.device)
+
+    # show the args.
+    logger.info("#########parameters settings....")
+    import config
+    log_settings(args, config.__dict__)
+
+    # save the current src
+    save_current_src(save_path = args.save_path, 
+                     src_path = SRC_PATH)
+
+if __name__ == "__main__":
+    main()
